528fb63ce875fa0cc2b51d484cf43af7446e073d
Nitishsi7 <nks1120q@gmail.com>
2025-06-26 06:25:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "25663773"
 api_hash = "211365a39963382b3048cf6586dcff61"
-bot_token = "7725512465:AAHHuN1Z17bmvEJM6Xqd21OdgcZqzTpbwBI"
+bot_token = "7725512465:AAF3z0NZJ8vSwslqoia7_DVTuvSnx-LktDA"
 auth_users = [7079591013]
